687fa031305f32f386482630acae2278183b22ff
Antoine <antoine@localhost.localdomain>
2017-06-13 16:11:42 +1000

    Corrected test 004

--- perf004/run.py
+++ perf004/run.py
@@ -2,11 +2,13 @@ import os
 from pysys.constants import *
 from pysys.basetest import BaseTest
 from pysys.process.helper import ProcessWrapper
-
+#from apama.common import ApamaServerProcess, _allocateUniqueProcessStdOutErr
+	
 class PySysTest(BaseTest):
         def execute(self):
             self.correlatorLog = os.path.join(self.output, 'correlator.log')
-            process = ProcessWrapper('correlator -f {}'.format(self.correlatorLog))
+
+            process = ProcessWrapper('/home/antoine/softwareag/Apama/bin/correlator', arguments=['-f', self.correlatorLog],environs=os.environ,workingDir=os.getcwd(), state=BACKGROUND, timeout=None)
             process.start()
             self.waitForFile(self.correlatorLog)
             process.stop()
