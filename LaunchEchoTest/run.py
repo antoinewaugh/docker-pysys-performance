f40a1b07c547e26dbb63c447794b3379b898796a
Antoine <antoine@localhost.localdomain>
2017-06-14 21:17:43 +1000

    Added launchechotest

---
+++ LaunchEchoTest/run.py
@@ -0,0 +1,12 @@
+from pysys.constants import *
+from pysys.basetest import BaseTest
+import subprocess, signal, os
+from pysys.process.helper import ProcessWrapper
+
+class PySysTest(BaseTest):
+        def execute(self):
+            process = ProcessWrapper('/usr/bin/echo', arguments=[],environs=os.environ,workingDir=os.getcwd(), state=FOREGROUND, timeout=None, stdout='/dev/null')
+            process.start()
+
+        def validate(self):
+            self.assertTrue(True)
